fad1f9525bb654deb2e23f7783e70e5731c997c3
Zeke Bickham <98800270+zbickham@users.noreply.github.com>
2022-03-24 08:12:45 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,2 +1,22 @@
 import pytest
 from app import *
+load_data()
+
+def test_epoch_data():
+    with pytest.raises(ValueError):
+        epoch_data('test')
+
+def test_country_data():
+    with pytest.raises(TypeError):
+        country_data('test')
+
+def test_region_data():
+    with pytest.raises(TypeError):
+        region_data('test')
+
+def test_city_data():
+    with pytest.raises(TypeError):
+        city_data('test')
+        
+def test_help():
+    assert isinstance(help(), str) == True
